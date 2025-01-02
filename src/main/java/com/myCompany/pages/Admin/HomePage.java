eaa09e2fec8347550853256da4ab1d116ff49180
newthevlash <newthevlash15@gmail.com>
2025-01-02 16:52:32 +0700

    Second commit fix feature and add feature

--- src/main/java/com/myCompany/pages/Admin/HomePage.java
+++ src/main/java/com/myCompany/pages/Admin/HomePage.java
@@ -1,6 +1,7 @@
 package com.myCompany.pages.Admin;
 
 import com.myCompany.drivers.DriverSingleton;
+import com.myCompany.utils.Utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -21,6 +22,19 @@ public class HomePage {
     @FindBy(css = ".css-uajqx8")
     private WebElement btnLogout;
 
+    @FindBy(xpath = "//p[normalize-space()='Laporan']")
+    private WebElement sidemenu_laporan;
+
+    @FindBy(xpath = "//p[normalize-space()='Semua']")
+    private WebElement sidemenu_semua;
+
+    @FindBy(xpath = "//p[normalize-space()='Import']")
+    private WebElement sidemenu_import;
+
+    @FindBy(xpath = "//p[normalize-space()='Import Status Aktif']")
+    private WebElement sidemenu_importStatusAktif;
+
+
 
 
 
@@ -36,6 +50,17 @@ public class HomePage {
         return driver.getCurrentUrl();
     }
 
+    public void goTOLaporanSemua(){
+        sidemenu_laporan.click();
+        Utils.delay(1);
+        sidemenu_semua.click();
+    }
+
+    public void goTOImportStatusAktif(){
+        sidemenu_import.click();
+        Utils.delay(1);
+        sidemenu_importStatusAktif.click();
+    }
 
 
 }
--- src/main/java/com/myCompany/utils/ScenarioTests.java
+++ src/main/java/com/myCompany/utils/ScenarioTests.java
@@ -17,10 +17,40 @@ public enum ScenarioTests {
     T11("Verify password masking while entering the password"),
     T12("Verify disabling password masking"),
     T13("Login fails when internet is offline"),
-    T14("Verify successful logout");
+    T14("Verify successful logout"),
 
-    // T14 - T1000000
-    // feature ......
+
+    // T15 - T32
+    // feature Laporan Semua
+    T15("Display data with select a valid date"),
+    T16("Displays Data With Input a Valid Name"),
+    T17("Admin Displays Data With a Valid Department Unit Filter"),
+    T18("Displays data with a valid name and date input"),
+    T19("Admin displays data with a valid name, date, and department unit filter"),
+    T20("Does not display data with an invalid name"),
+    T21("Does not display data with an invalid date"),
+    T22("Does not display data with an invalid department unit filter"),
+    T23("Does not display data with a valid name but an invalid date"),
+    T24("Does not display data with a valid name but an invalid department unit filter"),
+    T25("Does not display data with an invalid name but a valid date"),
+    T26("Does not display data with a valid date but an invalid department unit filter"),
+    T27("Does not display data with a valid name and date but an invalid department unit filter"),
+    T28("The data display does not change when the department name, date, and unit filters are empty"),
+    T29("Admin validates reset search fields"),
+    T30("Admin validates exiting the filter form"),
+    T31("Admin navigates between pages"),
+    T32("Admin changes rows per page"),
+
+
+    // T33 - T37
+    // feature Import Status Aktif
+
+    T33("Successfully import active status with a valid Excel file"),
+    T34("Download active status template file"),
+    T35("Fail to import active status with an invalid file format"),
+    T36("Fail to import active status without selecting a file"),
+    T37("Fail to import active status with a file that does not match the template");
+
 
     private String scenarioTestName;
 
